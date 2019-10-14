e5a15d4045cd1c460dcd981244894ca76426a8d5
Jaden37 <julespeguet.jp@gmail.com>
2019-10-14 18:32:50 +0200

    ajout vue material + creation 1er formulaire

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/theme/default.css'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="md-layout">
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Questionnaire</div>
+          </md-card-header>
+
+          <md-card-content>
+            <md-field>
+              <label>Nom</label>
+              <md-input v-model="JPnom" maxlength="40" required></md-input>
+            </md-field>
+            <md-field>
+              <label>Prénom</label>
+              <md-input v-model="JPprenom" maxlength="40" required></md-input>
+            </md-field>
+            <md-field>
+              <label>Société</label>
+              <md-input v-model="JPsociete" maxlength="50" required></md-input>
+            </md-field>
+          </md-card-content>
+
+          <md-card-actions>
+            <router-link to="/questionnaire"><md-button>Commencer le questionnaire</md-button></router-link>
+          </md-card-actions>
+        </md-card>
+      </div>
+      <div class="md-layout-item"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    maxLength: null,
+    JPnom: null,
+    JPprenom: null,
+    JPsociete: null
+  })
 }
+
 </script>
