72530d9f3aae049e919418cf1ba34867151e8e68
jules <jules04101999PEGUET>
2019-10-14 21:07:22 +0200

    add component question

---
+++ src/components/Question.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="question">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">{{JPQuestion}}</div>
+      </md-card-header>
+
+      <md-card-content>
+        <md-list>
+          <md-list-item>
+            <md-checkbox v-model="JPbool1">{{JPresponse1}}</md-checkbox>
+          </md-list-item>
+          <md-list-item>
+            <md-checkbox v-model="JPbool2">{{JPresponse2}}</md-checkbox>
+          </md-list-item>
+          <md-list-item>
+            <md-checkbox v-model="JPbool3">{{JPresponse3}}</md-checkbox>
+          </md-list-item>
+        </md-list>
+      </md-card-content>
+
+      <md-card-actions>
+        <router-link to="/questionnaire">
+          <md-button>Question suivante</md-button>
+        </router-link>
+      </md-card-actions>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    JPbool1: false,
+    JPbool2: false,
+    JPbool3: false
+  }),
+  props: {
+    JPQuestion: String,
+    JPresponse1: String,
+    JPresponse2: String,
+    JPresponse3: String
+  }
+}
+</script>
--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -2,43 +2,23 @@
   <div class="questionnaire">
     <div class="md-layout">
       <div class="md-layout-item"></div>
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Question N°</div>
-        </md-card-header>
-
-        <md-card-content>
-          <md-list>
-            <md-list-item>
-              <md-checkbox v-model="JPbool1">Réponse 1</md-checkbox>
-            </md-list-item>
-            <md-list-item>
-              <md-checkbox v-model="JPbool2">Réponse 2</md-checkbox>
-            </md-list-item>
-            <md-list-item>
-              <md-checkbox v-model="JPbool3">Réponse 3</md-checkbox>
-            </md-list-item>
-          </md-list>
-        </md-card-content>
-
-        <md-card-actions>
-          <router-link to="/questionnaire">
-            <md-button>Question suivante</md-button>
-          </router-link>
-        </md-card-actions>
-      </md-card>
+        <Question JPQuestion="Aimez vous les carottes ?" JPresponse1="OH OUIII" JPresponse2="Moyen" JPresponse3="Jamais de la vie"/>
+        <Question JPQuestion="Ta couleur favorite ?" JPresponse1="Vert" JPresponse2="Jaune" JPresponse3="Rouge"/>
       <div class="md-layout-item"></div>
     </div>
   </div>
 </template>
 
 <script>
+import Question from '@/components/Question.vue'
 export default {
-  name: "questionnaire",
+
+  name: 'questionnaire',
   data: () => ({
-    JPbool1: false,
-    JPbool2: false,
-    JPbool3: false
-  })
-};
+
+  }),
+  components: {
+    Question
+  }
+}
 </script>
